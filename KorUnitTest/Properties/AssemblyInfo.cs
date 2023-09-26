4b004e4378561af815e4b62885186cf210e71247
GergoDoka <dokagergo2005@gmail.com>
2023-09-26 12:57:29 +0200

    Kör Feladat

---
+++ KorUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KorUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KorUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("59761378-3607-46d6-8d28-79133c9caf67")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- Teszt/Program.cs
+++ Teszt/Program.cs
@@ -10,7 +10,16 @@ namespace Teszt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Teszt program");
+            //Példányosítom az osztályt
+            KorClass kor1 = new KorClass(3.0);
+            kor1.setKerulet(kor1.getSugar());
+            kor1.setTerulet();
+
+            Console.WriteLine("A {0:0.0000} sugarú kör Kerülete: {1:0.0000}. Területe: {2:0.0000}",
+                kor1.getSugar(),
+                kor1.getKerulet(),
+                kor1.getTerulet());
+
             Console.ReadKey();
         }
     }
