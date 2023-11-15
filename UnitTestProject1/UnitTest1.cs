057bed186b47dbcafcdd1f794c21fa6df2ef6e02
GergoDoka <dokagergo2005@gmail.com>
2023-11-15 11:38:33 +0100

    kész vagyok

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestClass]
+        public class KorTeszteles
+        {
+            [TestMethod]
+            public void TeruletTeszt()
+            {
+                // Arrange - Környezet beállítása
+                //Példányosítom az osztályt
+                NegyszogClass kr = new KorClass(3);
+                kr.setTerulet();
+
+                //Act - Futtatás
+                double varteredmeny = 28.27;
+                double kapotteredmeny = Math.Round(kr.getTerulet(), 2);
+                //Assert - Kiértékelés
+                Assert.AreEqual(varteredmeny, kapotteredmeny);
+            }
+            [TestMethod]
+            public void KeruletTeszt()
+            {
+                KorClass kr = new KorClass(3);
+                kr.setKerulet();
+                double varteredmeny = 18.85;
+                double kapotteredmeny = Math.Round(kr.getKerulet(), 2);
+                Assert.AreEqual(varteredmeny, kapotteredmeny);
+            }
+            [TestMethod]
+            public void SugarTeszt()
+            {
+                KorClass kr = new KorClass(3);
+                kr.getSugar();
+                double varteredmeny = 3;
+                double kapotteredmeny = kr.getSugar();
+                Assert.AreEqual(varteredmeny, kapotteredmeny);
+
+            }
+        }
+    }
+}
---
+++ kocka/NegyszogClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kocka
+{
+    class NegyszogClass
+    {
+        private double elsooldal, masodikoldal, kerulet, terulet;
+        public NegyszogClass(double a, double b)
+        {
+            this.elsooldal = a;
+            this.masodikoldal = b;
+        }
+        public void setKerulet(double a,double b)
+        {
+            this.terulet = 2*(a + b);
+        }
+        public void setTerulet(double a, double b)
+        {
+            this.terulet = a * b;
+        }
+        public double getKerulet()
+        {
+            return this.kerulet;
+        }
+        public double getTerulet()
+        {
+            return this.terulet;
+        }
+        public double getOldal()
+        {
+            return this.elsooldal;
+        }
+
+        public double getOldal2()
+        {
+            return this.masodikoldal;
+        }
+    }
+}
